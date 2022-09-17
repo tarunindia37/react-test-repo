278b2d3a3cca3512cc7abdaadfe36883c0655a4b
tarunindia37 <tarunindia37@gmail.com>
2022-09-17 16:55:15 +0530

    change the text

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Form from './components/Form';
 export default function App() {
   return (
     <div>
-      <h1>Form Example</h1>
+      <h1>Form Example with 2 ways binding</h1>
       <Form />
     </div>
   );
